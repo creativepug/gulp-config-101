ca6434bdd9254b1763184af62887dece46413f55
Armno <monkeyarmno@gmail.com>
2015-08-26 16:14:38 +0700

    now is the js time

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
 var gulp = require('gulp');
+
+// css related plugins
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+
+// javascript related plugins
+var jshint = require('gulp-jshint');
+var jscs = require('gulp-jscs');
+var concat = require('gulp-concat');
 
 var paths = {
 	css: {
 		src: ['./sass/**/*.scss'],
 		dest: './dist/css'
+	},
+	js: {
+		src: [
+			'./js/main.js'
+		],
+		dest: './dist/js'
 	}
 };
 
@@ -14,4 +27,17 @@ gulp.task('sass', function() {
 		.pipe(sass().on('error', sass.logError))
 		.pipe(prefix())
 		.pipe(gulp.dest(paths.css.dest));
+});
+
+gulp.task('js', function() {
+	return gulp.src(paths.js.src)
+		.pipe(jscs())
+			.on('error', function(err) {
+				console.log(err.toString());
+				this.emit('end');
+			})
+		.pipe(jshint('.jshintrc'))
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(concat('main.js'))
+		.pipe(gulp.dest(paths.js.dest));
 });
---
+++ js/main.js
@@ -0,0 +1,5 @@
+'use strict';
+
+function Hello() {
+	console.log('hello');
+}
